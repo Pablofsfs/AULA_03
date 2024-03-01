68a4a75fd4f191e4de442a7b42913469ac8ae436
Pablo Fernandes Sena <63509092+Pablofsfs@users.noreply.github.com>
2024-03-01 11:21:08 -0300

    commit banco

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
-const express = require("express")
-const app = express()
-
-
-app.listen(8081, function(){
-    console.log("Servidor Ativo!")
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize ("dwii","root","",{
+    host: "localhost", 
+    dialect: "mysql"
 })
 
-app.get("/", function(req, res){
-    console.log("Página Principal")
-    res.send("Pagina principal")
-})
-app.get("/contato", function(req, res){
-    console.log("Página Contato")
-    res.send("Página Contato")
-})
-
-app.get("/produtos/:item", function(req, res){
-    res.send("Página de produtos:" + req.params.item)
-})
-
-app.get("/carros/:modelo/:ano/:cor", function(req, res){
-    res.send("Página de carros: " + req.params.modelo + " ano: "+ req.params.ano + " cor: " + req.params.cor)
+sequelize.authenticate().then(function(){
+    console.log("Conectado com Sucesso!")
+}).catch(function(erro){
+    console.log(erro)
 })
